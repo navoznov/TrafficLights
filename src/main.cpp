fa136aef34a27e9e228bb2ccd285e45368e5587c
Navoznov.IE@parma.ru <Navoznov.IE@parma.ru>
2020-02-08 00:23:29 +0300

    эмуляция реальной работы 3х светофора в авторежиме

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,44 @@
 
 // Hardware settigs
 #define ledsCount 3 // 2 (красный/зеленый) или 3 (красный/желтый/зеленый)
+
 #define redLedPin 1
-#define yellowLedPin 4
-#define greenLedPin 2
-#define buttonPin 3
+#define yellowLedPin 2
+#define greenLedPin 3
+
+#define buttonPin 4
 
 // Software settings
-#define lightChangeTimeoutMs 2000
+#define LIGHT_CHANGE_TIMEOUT_MS 2000
+#define SWITCH_WITH_BLINKS true
 #define isButtonControl true
-#define isAutoControl true
+#define shouldStartInAutoMode true
+
+#define	RED_LIGHT_TIMEOUT_MS 10000
+#define	RED_YELLOW_LIGHT_TIMEOUT_MS 2000
+#define	GREEN_LIGHT_TIMEOUT_MS 10000
+#define GREEN_LIGHT_BLINK_TIMEOUT_MS 750
+#define	GREEN_LIGHT_BLINK_COUNT 3
+#define	YELLOW_LIGHT_TIMEOUT_MS 2000
 
 bool isRed = true;
+bool isYellow = false;
 bool isGreen = false;
+bool isGreenBlinking = false;
+
 bool isAutoMode = true;
 
 GButton button(buttonPin, LOW_PULL, NORM_OPEN);
-long lastLightChangeMs = 0;
+unsigned long nextLightChangeTimeMs = 0;
+
+void blinkLight(uint8_t pin, uint8_t count, int timeoutMs)
+{
+	for (size_t i = 0; i < count * 2; i++)
+	{
+		digitalWrite(pin, i % 2);
+		delay(timeoutMs);
+	}
+}
 
 void setNextLights()
 {
@@ -31,16 +53,35 @@ void setNextLights()
 	{
 		if (isRed)
 		{
-			isRed = false;
+			if (isYellow)
+			{
+				isRed = false;
+				isYellow = false;
+				isGreen = true;
+				nextLightChangeTimeMs = millis() + GREEN_LIGHT_TIMEOUT_MS;
+			}
+			else
+			{
+				isRed = true;
+				isYellow = true;
+				isGreen = false;
+				nextLightChangeTimeMs = millis() + RED_YELLOW_LIGHT_TIMEOUT_MS;
+			}
+		}
+		else if(isYellow)
+		{
+			isRed = true;
+			isYellow = false;
+			isGreen = false;
+			nextLightChangeTimeMs = millis() + RED_LIGHT_TIMEOUT_MS;
 		}
 		else if (isGreen)
 		{
-			isRed = true;
+			blinkLight(greenLedPin, GREEN_LIGHT_BLINK_COUNT, GREEN_LIGHT_BLINK_TIMEOUT_MS);
+			isRed = false;
+			isYellow = true;
 			isGreen = false;
-		}
-		else
-		{
-			isGreen = true;
+			nextLightChangeTimeMs = millis() + YELLOW_LIGHT_TIMEOUT_MS;
 		}
 	}
 }
@@ -50,15 +91,14 @@ void showCurrentLights()
 	digitalWrite(redLedPin, isRed ? HIGH : LOW);
 	if (ledsCount == 3)
 	{
-		digitalWrite(yellowLedPin, !isRed && !isGreen ? HIGH : LOW);
+		digitalWrite(yellowLedPin, isYellow ? HIGH : LOW);
 	}
 	digitalWrite(greenLedPin, isGreen ? HIGH : LOW);
 }
 
 void setup()
 {
-	isAutoMode = isAutoControl;
-	lastLightChangeMs = millis();
+	isAutoMode = shouldStartInAutoMode;
 
 	pinMode(redLedPin, OUTPUT);
 	if (ledsCount == 3)
@@ -66,6 +106,7 @@ void setup()
 		pinMode(yellowLedPin, OUTPUT);
 	}
 	pinMode(greenLedPin, OUTPUT);
+	nextLightChangeTimeMs = millis() + RED_LIGHT_TIMEOUT_MS;
 }
 
 void loop()
@@ -80,13 +121,12 @@ void loop()
 
 	if (isAutoMode)
 	{
-		if (millis() - lastLightChangeMs >= lightChangeTimeoutMs)
+		if (millis() >= nextLightChangeTimeMs)
 		{
-			lastLightChangeMs = millis();
 			setNextLights();
 		}
 	}
-	else if (button.isClick())
+	if (button.isClick())
 	{
 		setNextLights();
 	}
