3cfabc149ca3c73009b86ad177960bf5ffcaf8e4
Navoznov.IE@parma.ru <Navoznov.IE@parma.ru>
2020-02-17 19:46:31 +0300

    переделано переключение светодиодов, работает кнопка

--- src/main.cpp
+++ src/main.cpp
@@ -17,32 +17,32 @@
 #define SHOULD_START_IN_AUTO_MODE true
 
 #define RED_LIGHT_TIMEOUT_MS 10000
-#define RED_YELLOW_LIGHT_TIMEOUT_MS 2000
+#define RED_YELLOW_LIGHT_TIMEOUT_MS 2500
 #define GREEN_LIGHT_TIMEOUT_MS 10000
-#define GREEN_LIGHT_BLINK_TIMEOUT_MS 750
-#define GREEN_LIGHT_BLINK_COUNT 3
-#define YELLOW_LIGHT_TIMEOUT_MS 2000
+#define GREEN_LIGHT_BLINK_TIMEOUT_MS 600
+#define GREEN_LIGHT_BLINK_COUNT 6
+#define YELLOW_LIGHT_TIMEOUT_MS 2500
+
+#define LIGHTS_COUNT 5;
+uint8_t light = 0; // текущий режим: 0 красный, 1 красный и желтый, 2 зеленый, 3 мигающий зеленый, 4 желтый
 
 bool isRed = true;
 bool isYellow = false;
 bool isGreen = false;
-bool isGreenBlinking = false;
 
 bool isAutoMode = true;
-
-GButton button(BUTTON_PIN, LOW_PULL, NORM_OPEN);
+uint8_t greenBlinkingCounter = 0;
 unsigned long nextLightChangeTimeMs = 0;
 
-void blinkLight(uint8_t pin, uint8_t count, int timeoutMs)
+GButton button(BUTTON_PIN, LOW_PULL, NORM_OPEN);
+
+void gotoNextLight()
 {
-	for (size_t i = 0; i < count * 2; i++)
-	{
-		digitalWrite(pin, i % 2);
-		delay(timeoutMs);
-	}
+	// greenBlinkingCounter = 0;
+	// nextLightChangeTimeMs = 0;
 }
 
-void setNextLights()
+void setLights()
 {
 	if (LEDS_COUNT == 2)
 	{
@@ -51,38 +51,55 @@ void setNextLights()
 	}
 	else if (LEDS_COUNT == 3)
 	{
-		if (isRed)
-		{
-			if (isYellow)
-			{
-				isRed = false;
-				isYellow = false;
-				isGreen = true;
-				nextLightChangeTimeMs = millis() + GREEN_LIGHT_TIMEOUT_MS;
-			}
-			else
-			{
-				isRed = true;
-				isYellow = true;
-				isGreen = false;
-				nextLightChangeTimeMs = millis() + RED_YELLOW_LIGHT_TIMEOUT_MS;
-			}
-		}
-		else if (isYellow)
+		// текущий режим: 0 красный, 1 красный и желтый, 2 зеленый, 3 мигающий зеленый, 4 желтый
+		if (light == 0) // красный
 		{
 			isRed = true;
 			isYellow = false;
 			isGreen = false;
-			nextLightChangeTimeMs = millis() + RED_LIGHT_TIMEOUT_MS;
+			nextLightChangeTimeMs = millis() + RED_YELLOW_LIGHT_TIMEOUT_MS;
+			light++;
 		}
-		else if (isGreen)
+		else if (light == 1) // красный + желтый
 		{
-			blinkLight(GREEN_LED_PIN, GREEN_LIGHT_BLINK_COUNT, GREEN_LIGHT_BLINK_TIMEOUT_MS);
+			isRed = true;
+			isYellow = true;
+			isGreen = false;
+			nextLightChangeTimeMs = millis() + RED_YELLOW_LIGHT_TIMEOUT_MS;
+			light++;
+		}
+		else if (light == 2) // зеленый
+		{
+			isRed = false;
+			isYellow = false;
+			isGreen = true;
+			nextLightChangeTimeMs = millis() + GREEN_LIGHT_TIMEOUT_MS;
+			light++;
+		}
+		else if (light == 3) // зеленый мигающий
+		{
+			nextLightChangeTimeMs = millis() + GREEN_LIGHT_BLINK_TIMEOUT_MS;
+			if (greenBlinkingCounter >= GREEN_LIGHT_BLINK_COUNT)
+			{
+				greenBlinkingCounter = 0;
+				light++;
+			}
+			else
+			{
+				greenBlinkingCounter++;
+				isGreen = !isGreen;
+			}
+		}
+		else if (light == 4)
+		{
 			isRed = false;
 			isYellow = true;
 			isGreen = false;
 			nextLightChangeTimeMs = millis() + YELLOW_LIGHT_TIMEOUT_MS;
+			light++;
 		}
+
+		light = light % LIGHTS_COUNT;
 	}
 }
 
@@ -124,17 +141,17 @@ void loop()
 		return;
 	}
 
-	if (isAutoMode)
+	if (isAutoMode && millis() >= nextLightChangeTimeMs)
 	{
-		if (millis() >= nextLightChangeTimeMs)
-		{
-			setNextLights();
-		}
+		setLights();
 	}
 	if (button.isClick())
 	{
-		setNextLights();
+		// todo: подумать как избавтиься от этого костыля
+		if(light == 3) greenBlinkingCounter = GREEN_LIGHT_BLINK_COUNT;
+		setLights();
 	}
+	
 
 	showCurrentLights();
 }
